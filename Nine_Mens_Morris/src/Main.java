0533a5952a675ebde453c293c831677cbd6d8356
zhee0035 <zhee0035@student.monash.edu>
2023-04-22 09:56:19 +0800

    add JavaFX

--- Nine_Mens_Morris/src/Main.java
+++ Nine_Mens_Morris/src/Main.java
@@ -1,5 +1,26 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        System.out.println("Hello world!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        StackPane layout = new StackPane();
+
+        Button button = new Button("Click me");
+        button.setOnAction(e -> System.out.println("Hello world!"));
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 300, 250);
+        stage.setScene(scene);
+        stage.setTitle("JavaFX20");
+        stage.show();
     }
 }
