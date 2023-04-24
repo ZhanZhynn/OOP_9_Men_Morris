2adbc311554479c9c35ac6636b62b9cfd5abbe80
zhee0035 <zhee0035@student.monash.edu>
2023-04-24 11:01:44 +0800

    Add constructor for AI Player

--- Nine_Mens_Morris/src/game/Player/AIPlayer.java
+++ Nine_Mens_Morris/src/game/Player/AIPlayer.java
@@ -1,6 +1,15 @@
 package game.Player;
 
+import game.Utils.Colour;
+
 public class AIPlayer extends Player{
+
+    private String playerName;
+
+    public AIPlayer(String playerName, Colour colour) {
+        super(colour);
+        this.playerName = playerName;
+    }
 
 
 }
